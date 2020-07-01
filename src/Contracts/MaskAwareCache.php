04c9bdca3abb82bd45fcbf626ef743e9a28ca00b
Artem Prozorov <artem.g.prozorov@gmail.com>
2020-07-01 08:36:42 +0300

    Fixed contract for MaskAwareCache

--- src/Contracts/MaskAwareCache.php
+++ src/Contracts/MaskAwareCache.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Zebrains\Filecache\Contracts;
 
-interface MaskAwareCache
+use Psr\SimpleCache\CacheInterface;
+
+interface MaskAwareCache extends CacheInterface
 {
     /**
      * getByMask.
